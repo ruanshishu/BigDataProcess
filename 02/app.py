1390613e09d8496e93d4fcb952edb3fc14e65162
ruanshishu <166352435+ruanshishu@users.noreply.github.com>
2024-05-13 11:47:22 +0800

    Update app.py

--- 02/app.py
+++ 02/app.py
@@ -1,7 +1,7 @@
 
 import json,urllib.request
 
-url = 'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-AC53559C-E22B-44B0-B80A-AA40662B98A3&format=JSON'
+url = 'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=(API_KEY)&format=JSON'
 
 data = urllib.request.urlopen(url).read()
 output = json.loads(data)
